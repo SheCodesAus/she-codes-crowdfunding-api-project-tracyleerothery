244baf1acc1c54a7a8d23dc346292c936e0c52d9
Tracy Rothery <tracy.rothery@gmail.com>
2022-03-12 12:06:38 +0800

    update

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,12 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    goal = models.IntegerField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField()
+    owner = models.CharField(max_length=200)
