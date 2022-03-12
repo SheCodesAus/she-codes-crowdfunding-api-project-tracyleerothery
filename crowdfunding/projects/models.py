cf030c0595b7853e3555bc340464a1a85110bf81
Tracy Rothery <tracy.rothery@gmail.com>
2022-03-12 12:54:22 +0800

    Update_get_project_Post Project

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -7,6 +7,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     goal = models.IntegerField()
+    image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField()
     owner = models.CharField(max_length=200)
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
- 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from.models import Project
@@ -14,3 +13,19 @@ class ProjectList(APIView):
 
 
 
+    def post(self, request):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+class ProjectDetail(APIView):
+        
+        def get_object(self, pk):
+            return Project.objects.get(pk=pk)
+            
+        def get(self, request, pk):
+            project = self.get_object(pk)
+            serializer = ProjectSerializer(project)
+            return Response(serializer.data)
+        
