6a74c389f82b46392f317c2b89ecf7483c2dbb10
Tracy Rothery <tracy.rothery@gmail.com>
2022-03-21 21:40:30 +0800

    update_authentication_permissions
    Add a permission class to the project list so only logged in users can create new projects. authtoken to installed apps

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -18,6 +18,7 @@ class PledgeSerializer(serializers.Serializer):
 class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     title = serializers.CharField(max_length=200)
+    description = serializers.CharField(max_length=None)
     goal = serializers.IntegerField()
     image = serializers.URLField()
     is_open = serializers.BooleanField()
@@ -31,3 +32,14 @@ class ProjectSerializer(serializers.Serializer):
 
 class ProjectDetailSerializer(ProjectSerializer):
     pledges = PledgeSerializer(many=True, read_only=True)
+
+    def update(self, instance, validated_data):
+          instance.title = validated_data.get('title', instance.title)
+          instance.description = validated_data.get('description', instance.description)
+          instance.goal = validated_data.get('goal', instance.goal)
+          instance.image = validated_data.get('image', instance.image)
+          instance.is_open = validated_data.get('is_open', instance.is_open)
+          instance.date_created = validated_data.get('date_created', instance.date_created)
+          instance.owner = validated_data.get('owner', instance.owner)
+          instance.save()
+          return instance
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from.models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
 from django.http import Http404
 from rest_framework import status
+from .permissions import IsOwnerOrReadOnly
 
 class PledgeList(APIView):
+    
     
     def get(self, request):
         pledges = Pledge.objects.all()
@@ -26,6 +29,7 @@ class PledgeList(APIView):
 
 
 class ProjectList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         projects = Project.objects.all()
@@ -46,16 +50,35 @@ class ProjectList(APIView):
         )
 
 class ProjectDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
         
-        def get_object(self, pk):
-            try:
-                return Project.objects.get(pk=pk)
-            except Project.DoesNotExist:
-                raise Http404
+    def get_object(self, pk):
+        try:
+            # return Project.objects.get(pk=pk)
+            project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request,project)
+            return project
+        except Project.DoesNotExist:
+            raise Http404
 
-            
-        def get(self, request, pk):
-            project = self.get_object(pk)
-            serializer = ProjectDetailSerializer(project)
-            return Response(serializer.data)
         
+    def get(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectDetailSerializer(project)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=data,
+            partial=True
+        )
+        if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
