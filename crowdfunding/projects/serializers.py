1207f155bd44dc9668913f77eca3c2839bc56b8a
Tracy Rothery <tracy.rothery@gmail.com>
2022-03-22 20:39:11 +0800

    wip adding fields to serilizers

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,3 +1,5 @@
+from email.mime import image
+from unicodedata import category
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 from .models import Project, Pledge
@@ -7,8 +9,10 @@ class PledgeSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     amount = serializers.IntegerField()
     anonymous = serializers.BooleanField()
-    supporter = serializers.CharField(max_length=200)
+    # supporter = serializers.CharField(max_length=200)
     project_id = serializers.IntegerField()
+    # pledge_type = PledgeTypeSerializer(many=False, read_only=False)
+    supporter = serializers.ReadOnlyField(source='supporter.id')
 
     def create(self, validated_data):
         return Pledge.objects.create(**validated_data)
@@ -19,8 +23,12 @@ class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     title = serializers.CharField(max_length=200)
     description = serializers.CharField(max_length=None)
+    category = CategorySerializer(many=False, read_only=False)
     goal = serializers.IntegerField()
+    goal_date = serializers.DateTimeField()
+    progress = serializers.IntegerField()
     image = serializers.URLField()
+    status = serializers.CharField(max_length=200)
     is_open = serializers.BooleanField()
     date_created = serializers.DateTimeField()
     # pledges = PledgeSerializer(many=True, read_only=True)
@@ -31,6 +39,8 @@ class ProjectSerializer(serializers.Serializer):
         return Project.objects.create(**validated_data)
 
 class ProjectDetailSerializer(ProjectSerializer):
+    # description = serializers.CharField(max_length=200)
+    # image = serializers.URLField()
     pledges = PledgeSerializer(many=True, read_only=True)
 
     def update(self, instance, validated_data):
@@ -43,3 +53,12 @@ class ProjectDetailSerializer(ProjectSerializer):
           instance.owner = validated_data.get('owner', instance.owner)
           instance.save()
           return instance
+
+class CategorySerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    category_name = serializers.CharField(max_length=200)
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+# and a link within ProjectSerializer
+# category = CategorySerializer(many=False, read_only=False)
