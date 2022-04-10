f783d78b293c6805261fb1a4630017164136fb9a
Tracy Rothery <tracy.rothery@gmail.com>
2022-04-10 19:02:45 +0800

    Updated_urls

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.CustomUserList.as_view()),
-    path('<int:pk>/', views.CustomUserDetail.as_view()),
+    path('users', views.CustomUserList.as_view()),
+    path('users/<int:pk>/', views.CustomUserDetail.as_view()),
     path('register', views.RegisterView.as_view()),
 ]
 
