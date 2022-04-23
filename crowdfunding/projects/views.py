f387124349b9e18eabac88bb22802d137c6a465b
Tracy Rothery <tracy.rothery@gmail.com>
2022-04-23 12:01:50 +0800

    Update_error

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -95,7 +95,8 @@ class ProjectDetail(APIView):
         if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CategoryList(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
